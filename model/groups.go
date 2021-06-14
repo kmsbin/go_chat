5dc85a0e1ce5549b797587b4371506328a118146
kmsbin <kaulisabino@dataclick.com.br>
2021-06-14 17:19:49 -0300

    Remove duplicate struct

--- model/groups.go
+++ model/groups.go
@@ -7,12 +7,3 @@ type Group struct {
 	Name  string `gorm:"column:name"`
 	Users []User `gorm:"many2many:GroupsID"`
 }
-
-type User struct {
-	ID        uint    `gorm:"primarykey"`
-	ImageUrl  string  `gorm:"column:image_url"`
-	Name      string  `gorm:"column:name"`
-	Groups    []Group `gorm:"many2many:GroupsID"`
-	SecretKey uint    `gorm:"unique"`
-	gorm.Model
-}
